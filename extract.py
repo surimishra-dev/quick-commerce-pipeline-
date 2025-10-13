b6186f792a397e7d2f8d249c1dd0d943a467eb82
Abhranil Dasgupta <abhra.nil007@gmail.com>
2025-10-13 12:47:41 +0530

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,24 +1,24 @@
-import mysql.connector as mysql
-import MySQLdb
 import mysql.connector
 import pandas as pd
 
-db = mysql.connect = {
-    'host': '34.180.3.122',
-    'user': 'abhranil',
-    'password': 'Abhranil@89',
-    'database': 'QuickCommerce',
-    'port': 22
-}
+# Connect to MySQL
+db = mysql.connector.connect(
+    host="34.180.3.122",
+    user="abhranil",
+    password="Abhranil@89",
+    database="QuickCommerce",
+    port=3306  # MySQL default port, not 22 (22 is SSH)
+)
 
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
+# Create a cursor
 cur = db.cursor()
 
-# Use all the SQL you like
-cur.execute("SELECT * Orders")
-# print all the first cell of all the rows
-for row in cur.fetchall():
-    print(row[0])
+# Run your query
+cur.execute("SELECT * FROM Orders")
+
+# Fetch all rows
+rows = cur.fetchall()
+for row in rows:
+    print(row)
 
 db.close()
