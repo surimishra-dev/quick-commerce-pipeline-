3aee12d7b2e6cbb09e9105a30e94f7b0889d9bcd
Abhranil Dasgupta <abhra.nil007@gmail.com>
2025-10-15 20:34:41 +0530

    Update extractFirestoreData.py

--- extractFirestoreData.py
+++ extractFirestoreData.py
@@ -1,17 +1,26 @@
 from pymongo import MongoClient
 from urllib.parse import quote_plus
+import pandas as pd
+from google.cloud import storage
+import os
 
 # MongoDB-compatible Firestore connection details
 username = "abhranil"  # MongoDB username
 password = "ljcVOu0GqPUpc7PBnrsVCzr_Q9kFMy59NBFuiTrKf01_Lf0z"  # Your new password for the MongoDB user
 host = "89848341-4b54-406a-b047-a2e911f2ee64.asia-south1.firestore.goog"  # Firestore MongoDB endpoint
 database_name = "quick-commerce-inventory"  # Database name
+collection_name = "InventoryData"
 
 # URL-encode password
 encoded_password = quote_plus(password)
 
 # Build the MongoDB URI using the connection string
 uri = f"mongodb://{username}:{encoded_password}@{host}:443/{database_name}?loadBalanced=true&tls=true&authMechanism=SCRAM-SHA-256&retryWrites=false"
+
+#GCS bucket configuration
+CSV_FILE = "inventory_data.csv"
+BUCKET_NAME = "dataproc-staging-asia-south1-925894589695-qxkvzrhv"  # 🔹 Replace with your actual bucket name
+DESTINATION_BLOB_NAME = "data/inventory_data.csv
 
 # Create a MongoClient instance
 client = MongoClient(uri, serverSelectionTimeoutMS=10000)  # Timeout in milliseconds
@@ -35,3 +44,9 @@ try:
         
 except Exception as e:
     print("Failed to connect or fetch data:", e)
+
+ # Convert documents to Pandas DataFrame
+df = pd.DataFrame(docs)
+print("Print the Dataframe")
+print(df)
+
