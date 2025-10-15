f920058a94341f59682f4f0b2f5d1c7ef759073a
surimishra-dev <surimishra@gmail.com>
2025-10-15 15:49:28 +0530

    Update extract.py
    cc

--- extract.py
+++ extract.py
@@ -1,5 +1,5 @@
-#import mysql.connector
-#import pandas as pd
+
+import pandas as pd
 
 # Connect to MySQL
 #db = mysql.connector.connect(
@@ -24,15 +24,15 @@
 #db.close()
 
 
-import mySQLdb.connector
+import MySQLdb
 import pandas as pd
 
 try:
-    conn = mySQLdb.connect(
-        host="192.168.224.3",   # replace with your Cloud SQL public IP
-        user="abhranil",
-        password="Abhranil@89",
-        database="QuickCommerce",
+    conn = MySQLdb.connect(
+        host="10.29.208.3",   # replace with your Cloud SQL public IP
+        user="suryanshm",
+        password="Suri_mishra@12",
+        database="quick-commerce",
         port=3306
     )
 
@@ -42,11 +42,11 @@ try:
     df = pd.read_sql(query, conn)
     print(df.head())
 
-except mySQLdb.connector.Error as err:
+except MySQLdb.connect.Error as err:
     print(f"❌ Error: {err}")
 
-finally:
-    if conn.is_connected():
-        conn.close()
-        print("🔒 Connection closed.")
+# finally:
+#     if conn.is_connected():
+#         conn.close()
+#         print("🔒 Connection closed.")
 
