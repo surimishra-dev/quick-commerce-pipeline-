33d4edffc85cfbfd58f323f8172a466a5f4a3465
surimishra-dev <surimishra@gmail.com>
2025-10-23 12:00:11 +0530

    Update orders_transformation.py

--- orders_transformation.py
+++ orders_transformation.py
@@ -102,7 +102,7 @@ async def transform_inventory_data(spark):
     )
 
     # === Convert date fields to timestamp ===
-   inventory_df = inventory_df.withColumn("last_update", try_to_timestamp("last_update", "yyyy-MM-dd'T'HH:mm:ssX"))
+    inventory_df = inventory_df.withColumn("last_update", try_to_timestamp("last_update", "yyyy-MM-dd'T'HH:mm:ssX"))
 
 
     # === Flatten nested JSON (if coming from API) ===
